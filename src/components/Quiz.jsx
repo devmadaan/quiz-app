cfd1e9bd1881127ef75126723fc30ceadb3edc6a
devmadan <dev.madaan@nftically.com>
2024-03-23 19:15:02 +0530

    first

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import React, { useCallback, useState } from "react";
+import QUESTIONS from "../questions.js";
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswer] = useState([]);
+  console.log(userAnswers);
+  const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswer((previousAnswers) => {
+      return [...previousAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        questionIndex={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
